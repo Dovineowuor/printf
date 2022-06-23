0973aa9a1dd767775a1d06e4a1f44d42efc7e17f
owuordove@gmail.com <owuordove@gmail.com>
2022-06-23 11:16:14 +0300

    printf.c function

--- _printf.c
+++ _printf.c
@@ -1,82 +1,48 @@
-#include "main.h""
-#include <stdlib.h>
-
+#include "main.h"
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
- */
-static int (*check_for_specifiers(const char *format))(va_list)
-{
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
- */
+ * _printf - main function to print in console
+ * @format: array to print and check type
+ * Return: count of character printed
+ **/
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
+	int count = -1;
 
-	if (format == NULL)
-		return (-1);
-	va_start(valist, format);
-	while (format[i])
+	if (format != NULL)
 	{
-		for (; format[i] != '%' && format[i]; i++)
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
+		int i;
+		va_list values;
+		int (*savef)(va_list);
+
+		va_start(values, format);
+		if (format[0] == '%' && format[1] == '\0')
 			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
+
+		count = 0;
+
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count = count + _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					savef = get_func(format[i + 1]);
+					if (savef)
+						count = count + savef(values);
+					else
+						count += _putchar(format[i]) + _putchar(format[i + 1]);
+					i++;
+				}
+			}
+			else
+				count = count + _putchar(format[i]);
+		}
+		va_end(values);
 	}
-	va_end(valist);
-	return (count);
+return (count);
 }
