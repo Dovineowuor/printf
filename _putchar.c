128d457ab340b1f8e09f887a8fe25164df2859a3
owuordove@gmail.com <owuordove@gmail.com>
2022-06-23 11:16:31 +0300

    putchar function

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,8 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * @c: Store the character in ascii
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
