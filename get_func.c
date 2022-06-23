01eda1a665ae1e24f63b334d7e9cf2f0dc781d5e
owuordove@gmail.com <owuordove@gmail.com>
2022-06-23 11:17:10 +0300

    get  function

---
+++ get_func.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_func - search and return the correct function
+ * @s: array to check
+ * Return: return a function
+ **/
+
+int (*get_func(char s))(va_list)
+{
+	op_t ops[] = {
+		{"c", _printChar},
+		{"s", _printString},
+		{"d", _printDec},
+		{"i", _printInt},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	for (i = 0; ops[i].c != NULL; i++)
+	{
+		if (*ops[i].c == s)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
